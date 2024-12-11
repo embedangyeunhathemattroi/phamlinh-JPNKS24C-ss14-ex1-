2ee54e00bddc0773d4757feb7f2a27ae8dabfa59
empedangiunhatptit <phamngoclinhtq2006@gmail.com>
2024-12-11 11:30:01 +0700

    Add files via upload

---
+++ ex1-ss14.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+char word[100];
+   printf("Moi ban nhap vao chuoi ky tu : \n",word);
+   gets(word);
+   printf("ky tu ban vua nhap la :%s \n", word);
+   
+return 0;
+}
+
